47c420704548fad5a8f3c7258150b91093c3da1c
erick <eampineda.0392@gmail.com>
2022-11-15 13:50:27 -0600

    Se agrega el proyecto

---
+++ src/test/java/com/ecodeup/apirest/PruebaApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecodeup.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
